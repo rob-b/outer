c42532d64d99af77229d06ce04d81a5a41bbed9d
Rob Berry <rob@luvhate.us>
2017-04-24 23:02:57 +0100

    Keep ghc-mod process alive

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Lib where
- 
+
 import           Control.Concurrent            (forkFinally, threadDelay)
 import           Control.Concurrent.STM        (atomically)
 import           Control.Concurrent.STM.TMChan
 import qualified Control.Exception             as X
 import           Control.Monad                 (forever, when)
+import           Control.Monad.Trans.State
 import           Control.Monad.Trans.Class     (lift)
 import           Control.Monad.Trans.Except    (runExceptT, throwE)
 import           Data.List                     (isPrefixOf)
@@ -21,6 +22,7 @@ import           System.Process
 import           System.Process.Internals
 
 
+--------------------------------------------------------------------------------
 getPid :: ProcessHandle -> IO (Maybe PHANDLE)
 getPid ph = withProcessHandle ph unpack
   where
@@ -30,6 +32,7 @@ getPid ph = withProcessHandle ph unpack
         ClosedHandle _ -> return Nothing
 
 
+--------------------------------------------------------------------------------
 mkProcess :: IO (Handle, Handle, Handle, ProcessHandle)
 mkProcess = do
   let p =
@@ -47,10 +50,12 @@ mkProcess = do
   return (hin, hout, herr, ph)
 
 
+--------------------------------------------------------------------------------
 sleep :: Int -> IO ()
 sleep x = threadDelay $ x * 1000
 
 
+--------------------------------------------------------------------------------
 readUntilOk :: Handle -> [String] -> IO [String]
 readUntilOk handle acc = do
   result <- X.try (hGetLine handle) :: IO (Either X.IOException String)
@@ -65,69 +70,67 @@ readUntilOk handle acc = do
         else readUntilOk handle (line : acc)
 
 
-go :: TMChan String -> TMChan String -> IO ()
-go inChan outChan = do
-  (hin,hout,_,ph) <- mkProcess
-  putStrLn "Made new process"
-  go' hin hout ph
+--------------------------------------------------------------------------------
+go :: TMChan String -> TMChan String -> StateT GhcMod IO ()
+go inChan outChan = forever go'
   where
-    go' hin' hout' ph' = do
-      msgM <- atomically $ readTMChan inChan
+    go' = do
+      msgM <- lift $ atomically $ readTMChan inChan
       case msgM of
         Nothing -> error "Channel is closed. Do something!"
         Just msg -> do
-          exitCodeM <- getProcessExitCode ph'
-          putStrLn $ maybe "ghc-mod is still running" (\ec -> "exit code: " <> show ec) exitCodeM
-          case exitCodeM of
-            Just _ -> do
-              hClose hin'
-              hClose hout'
-              return ()
-            Nothing -> do
-              hPutStrLn hin' msg >> readUntilOk hout' [] >>=
-                \results -> atomically $ writeTMChan outChan (unlines (reverse results))
-              go' hin' hout' ph'
+          ghcmod <- get
+          ghcmod' <- lift $ getGhcProcess ghcmod
+          put ghcmod'
+          lift $
+            hPutStrLn (stdinHandle ghcmod') msg >>
+            readUntilOk (stdoutHandle ghcmod') [] >>=
+            \results ->
+               atomically $ writeTMChan outChan (unlines (reverse results))
 
 
-data GhcMod = GhcMod
-  { stdoutHandle :: Handle
-  , stdinHandle :: Handle
-  , processHandle :: ProcessHandle
-  }
+--------------------------------------------------------------------------------
+data GhcMod
+  = DeadGhcMod
+  | ActiveGhcMod { stdoutHandle :: Handle
+                 , stdinHandle :: Handle
+                 , processHandle :: ProcessHandle}
+
+--------------------------------------------------------------------------------
+instance Show GhcMod where
+  show DeadGhcMod = "non-running ghc-mod"
+  show ActiveGhcMod{} = "running ghc-mod"
 
 
-main :: IO ()
-main =
-  withSocketsDo $
-  do sock <- socket AF_INET Stream 0
-     setSocketOption sock ReuseAddr 1
-     bind sock (SockAddrInet port iNADDR_ANY)
-     listen sock 2
-     putStrLn $ "listening on port " <> show port
-     forever $
-       do (conn,_) <- accept sock
-          handle <- socketToHandle conn ReadWriteMode
-          forkFinally
-            (talk handle)
-            (\eE ->
-                putStrLn (either show (const "no errors from talk") eE) >>
-                putStrLn "The talk process died" >>
-                hClose handle)
-
-
+--------------------------------------------------------------------------------
 port :: PortNumber
 port = 4444
 
 
-talk :: Handle -> IO ()
+--------------------------------------------------------------------------------
+getGhcProcess :: GhcMod -> IO GhcMod
+getGhcProcess DeadGhcMod = mkGhcMod
+getGhcProcess ghcmod@(ActiveGhcMod stdout stdin phandle) = do
+  exitCodeM <- getProcessExitCode phandle
+  case exitCodeM of
+    Nothing -> return ghcmod
+    Just _ -> do
+      hClose stdout
+      hClose stdin
+      mkGhcMod
+
+
+--------------------------------------------------------------------------------
+talk :: Handle -> StateT GhcMod IO ()
 talk h = do
-  hSetBuffering h LineBuffering
-  inChan <- newTMChanIO
-  outChan <- newTMChanIO
-  forkFinally
-    (go inChan outChan)
+  ghcmod <- get
+  lift $ hSetBuffering h LineBuffering
+  inChan <- lift newTMChanIO
+  outChan <- lift newTMChanIO
+  lift $ forkFinally
+    (runStateT (go inChan outChan) ghcmod)
     (\_ -> atomically (closeTMChan inChan) >> atomically (closeTMChan outChan))
-  _ <- fmap (either id id) $ runExceptT $ forever $ loop inChan outChan
+  _ <- lift $ fmap (either id id) $ runExceptT $ forever $ loop inChan outChan
   return ()
   where
     loop inChan' outChan' =
@@ -140,3 +143,42 @@ talk h = do
             lift $ putStrLn $ maybe "outChan is closed" (\r -> "result: " <> r) res'
             when (isNothing res') $ throwE ()
           else lift $ putStrLn "Unknown command"
+
+
+--------------------------------------------------------------------------------
+doTalk :: Socket -> StateT GhcMod IO ()
+doTalk sock = do
+  ghcmod <- get
+  forever $ do
+        (conn,_) <- lift $ accept sock
+        handle <- lift $ socketToHandle conn ReadWriteMode
+        lift $ forkFinally
+          (runStateT (talk handle) ghcmod)
+          (\_ -> hClose handle)
+
+
+--------------------------------------------------------------------------------
+mkGhcMod :: IO GhcMod
+mkGhcMod = do
+  (hin,hout,_,ph) <- mkProcess
+  return
+    ActiveGhcMod
+    { stdoutHandle = hout
+    , stdinHandle = hin
+    , processHandle = ph
+    }
+
+
+--------------------------------------------------------------------------------
+main :: IO ()
+main =
+  withSocketsDo $
+  do sock <- socket AF_INET Stream 0
+     setSocketOption sock ReuseAddr 1
+     bind sock (SockAddrInet port iNADDR_ANY)
+     listen sock 2
+     putStrLn $ "listening on port " <> show port
+     ghcmod <- mkGhcMod
+     putStrLn "Made new process"
+     runStateT (doTalk sock) ghcmod
+     return ()
