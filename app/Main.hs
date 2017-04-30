9d0f9c2fdb76ea94d48a94c4adcb2ebdbd51546e
Rob Berry <rob@luvhate.us>
2017-04-30 22:19:03 +0100

    Use retry and pool to manage ghc-mod

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 import qualified Lib
+import Outer(run)
 
 
 main :: IO ()
-main = Lib.main
+-- main = Lib.main
+main = run
--- src/Outer.hs
+++ src/Outer.hs
@@ -1,14 +1,27 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Outer where
 
--- import Control.Retry
-import System.Process
-       (ProcessHandle, std_in, std_out, std_err, createProcess, proc,
-        StdStream(CreatePipe))
-import System.IO
-       (Handle, BufferMode(LineBuffering), hSetBuffering, hSetBinaryMode)
-import           Data.Monoid                   ((<>))
-import Network.Socket
+import qualified Control.Exception as X
+import           Control.Monad     (forever, when)
+import           Control.Retry     (RetryPolicyM, constantDelay, limitRetries,
+                                    retrying)
+import           Data.List         (isPrefixOf)
+import           Data.Maybe
+import           Data.Monoid       ((<>))
+import           Data.Pool         (LocalPool, Pool, createPool,
+                                    destroyResource, putResource, takeResource)
+import           System.Exit       (ExitCode)
+import           System.IO         (BufferMode (..), Handle, hGetLine,
+                                    hPutStrLn, hSetBinaryMode, hSetBuffering,
+                                    hSetEncoding, stdin, stdout, utf8)
+import           System.IO.Error   (isEOFError)
+import           System.Process    (ProcessHandle, StdStream (CreatePipe),
+                                    createProcess, getProcessExitCode, proc,
+                                    std_err, std_in, std_out, terminateProcess)
+
+
+killGhcMod :: GhcMod -> IO ()
+killGhcMod (ActiveGhcMod _ _ processHandle') = terminateProcess processHandle'
 
 
 --------------------------------------------------------------------------------
@@ -30,6 +43,14 @@ mkProcess = do
 
 
 --------------------------------------------------------------------------------
+data GhcMod = ActiveGhcMod
+  { stdoutHandle :: Handle
+  , stdinHandle :: Handle
+  , processHandle :: ProcessHandle
+  }
+
+
+--------------------------------------------------------------------------------
 mkGhcMod :: IO GhcMod
 mkGhcMod = do
   (hin,hout,_,ph) <- mkProcess
@@ -42,28 +63,56 @@ mkGhcMod = do
 
 
 --------------------------------------------------------------------------------
-data GhcMod = ActiveGhcMod
-  { stdoutHandle :: Handle
-  , stdinHandle :: Handle
-  , processHandle :: ProcessHandle
-  }
+readUntilOk :: Handle -> [String] -> IO [String]
+readUntilOk handle acc = do
+  result <- X.try (hGetLine handle) :: IO (Either X.IOException String)
+  case result of
+    Left err ->
+      if isEOFError err
+        then return acc
+        else readUntilOk handle acc
+    Right line ->
+      if "OK" `isPrefixOf` line
+        then return $ line : acc
+        else readUntilOk handle (line : acc)
 
 
 --------------------------------------------------------------------------------
-port :: PortNumber
-port = 4444
+run :: IO ()
+run = do
+  hSetBuffering stdin LineBuffering
+  hSetEncoding  stdin utf8
 
+  hSetBuffering stdout LineBuffering
+  hSetEncoding  stdout utf8
+  pool <- createPool mkGhcMod killGhcMod 1 10 1
+  forever $ poolio pool
 
---------------------------------------------------------------------------------
-main :: IO ()
-main =
-  withSocketsDo $
-  do sock <- socket AF_INET Stream 0
-     setSocketOption sock ReuseAddr 1
-     bind sock (SockAddrInet port iNADDR_ANY)
-     listen sock 2
-     putStrLn $ "listening on port " <> show port
-     ghcmod <- mkGhcMod
-     putStrLn "Made new process"
-     -- runStateT (_) ghcmod
-     return ()
+  where
+    def :: RetryPolicyM IO
+    def = constantDelay 50000 <> limitRetries 2
+
+    ghcCheck :: Pool GhcMod -> (GhcMod, LocalPool GhcMod) -> IO Bool
+    ghcCheck pool (resource,local) = do
+      exited' <- isJust <$> exited resource
+      when exited' $ destroyResource pool local resource
+      return exited'
+
+    poolio :: Pool GhcMod -> IO ()
+    poolio pool = do
+      -- FIXME: add catch-all exception handler that destroys resource
+      (resource,local) <-
+        retrying def (\_ pair -> ghcCheck pool pair) (\_ -> takeResource pool)
+      ret <- action resource
+      putResource local resource
+      return ret
+
+    action :: GhcMod -> IO ()
+    action ghc = do
+      userInput <- getLine
+      hPutStrLn (stdinHandle ghc) userInput
+      result <- readUntilOk (stdoutHandle ghc) []
+      putStrLn . unlines $ reverse result
+
+    exited :: GhcMod -> IO (Maybe ExitCode)
+    exited (ActiveGhcMod _ _ processHandle') = getProcessExitCode processHandle'
