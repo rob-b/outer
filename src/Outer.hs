b62d432e8af795c6cd05c37eba08788da833083a
Rob Berry <rob@luvhate.us>
2017-05-01 22:52:34 +0100

    Run tcp server that passes messages to ghc-mod

--- src/Outer.hs
+++ src/Outer.hs
@@ -1,23 +1,83 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Outer where
 
-import qualified Control.Exception as X
-import           Control.Monad     (forever, when)
-import           Control.Retry     (RetryPolicyM, constantDelay, limitRetries,
-                                    retrying)
-import           Data.List         (isPrefixOf)
-import           Data.Maybe
-import           Data.Monoid       ((<>))
-import           Data.Pool         (LocalPool, Pool, createPool,
-                                    destroyResource, putResource, takeResource)
-import           System.Exit       (ExitCode)
-import           System.IO         (BufferMode (..), Handle, hGetLine,
-                                    hPutStrLn, hSetBinaryMode, hSetBuffering,
-                                    hSetEncoding, stdin, stdout, utf8)
-import           System.IO.Error   (isEOFError)
-import           System.Process    (ProcessHandle, StdStream (CreatePipe),
-                                    createProcess, getProcessExitCode, proc,
-                                    std_err, std_in, std_out, terminateProcess)
+import Control.Monad.Trans.Control (control)
+import           Control.Concurrent            (forkIO)
+import           Control.Concurrent.STM        (atomically)
+import           Control.Concurrent.STM.TMChan
+import qualified Control.Exception             as X
+import           Control.Monad                 (forever, when)
+import           Control.Monad.Trans.Class     (lift)
+import           Control.Monad.Trans.Maybe
+import           Control.Retry                 (RetryPolicyM, constantDelay,
+                                                limitRetries, retrying)
+import qualified Data.ByteString.Char8         as BC8
+import           Data.List                     (isPrefixOf)
+import           Data.Maybe                    (isJust, fromMaybe)
+import           Data.Monoid                   ((<>))
+import           Data.Pool                     (LocalPool, Pool, createPool,
+                                                destroyResource, putResource,
+                                                takeResource)
+import           System.Exit                   (ExitCode)
+import           System.IO                     (BufferMode (..), Handle,
+                                                hGetLine, hPutStrLn,
+                                                hSetBinaryMode, hSetBuffering)
+import           System.IO.Error               (isEOFError)
+import           System.Process                (ProcessHandle,
+                                                StdStream (CreatePipe),
+                                                createProcess,
+                                                getProcessExitCode, proc,
+                                                std_err, std_in, std_out,
+                                                terminateProcess)
+import           System.Socket
+import           System.Socket.Family.Inet6
+import           System.Socket.Protocol.TCP
+import           System.Socket.Type.Stream
+import           System.Timeout                (timeout)
+
+
+talk :: TMChan String -> IO c
+talk inChan =
+  X.bracket
+    (socket :: IO (Socket Inet6 Stream TCP))
+    (\s -> do
+       close s
+       putStrLn "Listening socket closed.")
+    (\s -> do
+       setSocketOption s (ReuseAddress True)
+       setSocketOption s (V6Only False)
+       bind s (SocketAddressInet6 inet6Any 8080 0 0)
+       listen s 5
+       putStrLn "Listening socket ready..."
+       forever $ acceptAndHandle s inChan `X.catch` \e -> print (e :: SocketException))
+
+
+timeMilli :: Num a => a -> a
+timeMilli n = n * 1000
+
+
+acceptAndHandle :: Socket Inet6 Stream TCP -> TMChan String -> IO ()
+acceptAndHandle s inChan =
+  X.bracket
+    (accept s)
+    (\(p, _addr) -> close p)
+    (\(p,_addr) -> do
+       msg <- timeout (timeMilli 300000) $ receive p 1024 msgNoSignal
+       cmd <- processCmd msg inChan
+       let msg' = fromMaybe "either timed out or invalid command" cmd
+       sendAll p msg' msgNoSignal
+       return ())
+
+
+processCmd :: Maybe BC8.ByteString -> TMChan String -> IO (Maybe BC8.ByteString)
+processCmd msg chan =
+  let isKnown = maybe False ("check" `BC8.isPrefixOf`)
+  in (if isKnown msg then
+     runMaybeT $
+       do msg' <- MaybeT $ return msg
+          lift . atomically $ writeTMChan chan (BC8.unpack msg')
+          return msg'
+     else return Nothing)
 
 
 killGhcMod :: GhcMod -> IO ()
@@ -78,19 +138,14 @@ readUntilOk handle acc = do
 
 
 --------------------------------------------------------------------------------
-run :: IO ()
-run = do
-  hSetBuffering stdin LineBuffering
-  hSetEncoding  stdin utf8
-
-  hSetBuffering stdout LineBuffering
-  hSetEncoding  stdout utf8
+ghcModCommunicate :: TMChan String -> TMChan String -> IO ()
+ghcModCommunicate inChan outChan = do
   pool <- createPool mkGhcMod killGhcMod 1 10 1
-  forever $ poolio pool
+  forever $ poolio pool (action inChan outChan)
 
   where
     def :: RetryPolicyM IO
-    def = constantDelay 50000 <> limitRetries 2
+    def = constantDelay 250000 <> limitRetries 2
 
     ghcCheck :: Pool GhcMod -> (GhcMod, LocalPool GhcMod) -> IO Bool
     ghcCheck pool (resource,local) = do
@@ -98,21 +153,47 @@ run = do
       when exited' $ destroyResource pool local resource
       return exited'
 
-    poolio :: Pool GhcMod -> IO ()
-    poolio pool = do
-      -- FIXME: add catch-all exception handler that destroys resource
-      (resource,local) <-
-        retrying def (\_ pair -> ghcCheck pool pair) (\_ -> takeResource pool)
-      ret <- action resource
-      putResource local resource
-      return ret
+    poolio :: Pool GhcMod -> (GhcMod -> IO ()) -> IO ()
+    poolio pool fn =
+      control $
+      \runInIO ->
+         X.mask $
+         \restore -> do
+           (resource,local) <- 
+             retrying def (\_ pair -> ghcCheck pool pair) (\_ -> takeResource pool)
+           ret <- 
+             restore (runInIO (fn resource)) `X.onException`
+             destroyResource pool local resource
+           putResource local resource
+           return ret
 
-    action :: GhcMod -> IO ()
-    action ghc = do
-      userInput <- getLine
-      hPutStrLn (stdinHandle ghc) userInput
+    action :: TMChan String -> TMChan String -> GhcMod -> IO ()
+    action inChan' outChan' ghc = do
+      putStrLn "Doing action..."
+      userInputM <- atomically $ readTMChan inChan'
+      case userInputM of
+        Nothing -> print "Channel is closed" >> error "Channel is closed"
+        Just userInput -> hPutStrLn (stdinHandle ghc) (trim userInput)
       result <- readUntilOk (stdoutHandle ghc) []
-      putStrLn . unlines $ reverse result
+      atomically $ writeTMChan outChan' (unlines $ reverse result)
+      where trim = reverse . dropWhile (=='\n') . reverse
 
     exited :: GhcMod -> IO (Maybe ExitCode)
     exited (ActiveGhcMod _ _ processHandle') = getProcessExitCode processHandle'
+
+
+run :: IO ()
+run = do
+  inChan <- newTMChanIO
+  outChan <- newTMChanIO
+  forkIO $ talk inChan
+  forkIO . forever $ consume outChan
+  ghcModCommunicate inChan outChan
+
+
+consume :: TMChan String -> IO ()
+consume outChan = do
+  msgM <- atomically $ readTMChan outChan
+  case msgM of
+    Nothing -> error "Cannot consume from closed channel"
+    Just result -> putStrLn result
